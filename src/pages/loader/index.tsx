923a73dbefcbfd64d14fee97973951bf8b148b29
xiref <felixcriollovigo@gmail.com>
2022-09-16 21:17:42 -0500

    UI loader

--- src/pages/loader/index.tsx
+++ src/pages/loader/index.tsx
@@ -1,10 +1,36 @@
 import { Wrapper } from "../../styled-components/utils";
 import Logo from "../../assets/logo";
+import { LoaderCOntainer, Spin, SpinContainer } from "./styles";
+
+type spinerProps = {
+  id: number;
+  type: "default" | "focus"
+}
+
+const spinerList: spinerProps[] = [
+  { id: 1, type: "focus" },
+  { id: 2, type: "default" },
+  { id: 3, type: "default" },
+  { id: 4, type: "default" },
+  { id: 5, type: "default" },
+  { id: 6, type: "default" },
+  { id: 7, type: "default" },
+]
 
 export function Loader() {
   return (
     <Wrapper>
-      <Logo />
+      <LoaderCOntainer>
+        <Logo />
+        
+        <SpinContainer>
+          {spinerList.map((spiner) => 
+            (<Spin 
+              key={spiner.id} 
+              type={spiner.type} />)
+          )}
+        </SpinContainer>
+      </LoaderCOntainer>
     </Wrapper>
   )
 }
--- src/pages/loader/styles.ts
+++ src/pages/loader/styles.ts
@@ -1,1 +1,44 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
+
+export const LoaderCOntainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const SpinContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  position: relative;
+`
+
+type SpinProps = {
+  type?: "default" | "focus"
+}
+
+export const Spin = styled.span<SpinProps>`
+  background: ${props => props.type === "focus" ? props.theme.colors.main : "transparent"};
+  border: 1px solid ${props => props.theme.colors.stroke};
+  width: .8em;
+  height: .8em;
+  border-radius: 100px;
+  
+  ${props => 
+    props.type === "focus" 
+      ? spinningAnimation 
+      : null
+  }
+`
+
+const spinning = keyframes`
+  0% {left: 21px;}
+  20% {left: 55px;}
+  40% {left: 89px;}
+  60% {left: 123px;}
+  80% {left: 157px;}
+  100% {left: 191px;}
+`
+
+const spinningAnimation = css`
+  position: absolute;
+  animation: ${spinning} 3s infinite;
+`
